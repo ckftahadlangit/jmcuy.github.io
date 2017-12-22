65730d3d98dc6161e1f35bd1162e349c09abd478
jmcuy <thegirlwholovesthesky@gmail.com>
2017-12-23 01:35:09 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCld-zlyyysUfAOw5CYj0Cn8w0QJAF0qPU",
+  authDomain: "cmsc127-926a8.firebaseapp.com",
+  databaseURL: "https://cmsc127-926a8.firebaseio.com",
+  projectId: "cmsc127-926a8",
+  storageBucket: "cmsc127-926a8.appspot.com",
+  messagingSenderId: "421673909250"
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log("Auth state changed")
+  if(user){
+    window.location = "wall.html"
+
+  }
+});
